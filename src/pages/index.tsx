3dc35e54e4bea35502f9c5191f99c5987252356c
nittashiori <nitta@impv.co.jp>
2022-08-25 16:38:56 +0900

    ボタンをクリックした時にAPIで猫画像を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 interface CatCategory {
   id: number;
@@ -16,43 +16,27 @@ interface SearchCatImage {
 
 type SearchCatImageResponse = SearchCatImage[];
 
-const catImages: string[] = [
-  "https://cdn2.thecatapi.com/images/bpc.jpg",
-  "https://cdn2.thecatapi.com/images/eac.jpg",
-  "https://cdn2.thecatapi.com/images/6qi.jpg"
-]
-
-const randomCatImage = (): string => {
-  const index = Math.floor(Math.random() * catImages.length);
-  return catImages[index];
-};
-
 const fetchCatImage = async (): Promise<SearchCatImage> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const result = (await res.json()) as SearchCatImageResponse;
   return result[0];
 }
 
-fetchCatImage().then((image) => {
-  console.log(`猫の画像: ${image.url}`);
-})
-
 const IndexPage = () => {
-  const [catImage, setCatImage] = useState<string | undefined>(undefined);
+  const [catImageUrl, setCatImageUrl] = useState(
+    "https://cdn2.thecatapi.com/images/bpc.jpg"
+  );
 
-  useEffect(() => {
-    setCatImage(randomCatImage());
-  })
-
-  const handleClick = () => {
-    setCatImage(randomCatImage());
+  const handleClick = async () => {
+    const image = await fetchCatImage();
+    setCatImageUrl(image.url);
   }
 
   return (
     <div>
       <button onClick={handleClick}>きょうのにゃんこ🐱</button>
       <div style={{ marginTop: 8 }}>
-        <img src={catImage} />
+        <img src={catImageUrl} />
       </div>
     </div>
   );
